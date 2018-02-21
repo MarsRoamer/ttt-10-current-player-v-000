c5235e32e063b8e93d94886f20abe7e8efcd6cba
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-21 20:39:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,9 @@ count
 end
 
 def current_player(board)
-  turn_count(board)
-    if count % 2 == 0
-      "X"
-    else
-      "O"
-    end
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
 end
